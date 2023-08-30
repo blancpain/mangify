cd7cb31002830b5502e34290bbf47956661627a4
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-08-30 23:32:25 +0300

    Add meal carousel and features overview components

--- client/src/features/landing-page/components/Hero.tsx
+++ client/src/features/landing-page/components/Hero.tsx
@@ -4,8 +4,6 @@ import heroBg from '../../../assets/hero-bg.jpg';
 const useStyles = createStyles((theme) => ({
   hero: {
     position: 'relative',
-    // backgroundImage:
-    //   'url(https://images.unsplash.com/photo-1555243896-c709bfa0b564?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80)',
     backgroundImage: `url(${heroBg})`,
     backgroundAttachment: 'fixed',
     backgroundSize: 'cover',
--- client/src/features/landing-page/components/MealGenerator.tsx
+++ client/src/features/landing-page/components/MealGenerator.tsx
@@ -1,7 +1,10 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { createStyles, rem, Select, Flex, Button, Text } from '@mantine/core';
+import { createStyles, rem, Select, Flex, Button, Text, Title } from '@mantine/core';
 import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import { MealGeneratorLandingSchema, TMealGeneratorLandingSchema } from '@shared/types';
+import { useState } from 'react';
+import { MealsCarousel } from './MealsCarousel';
+import { FeaturesOverview } from './FeaturesOverview';
 
 const useStyles = createStyles((theme) => ({
   root: {
@@ -12,6 +15,10 @@ const useStyles = createStyles((theme) => ({
     height: rem(94),
     paddingTop: rem(30),
     width: '350px',
+
+    [theme.fn.smallerThan('sm')]: {
+      width: '100%',
+    },
   },
 
   label: {
@@ -22,10 +29,17 @@ const useStyles = createStyles((theme) => ({
     paddingTop: `calc(${theme.spacing.sm} / 2)`,
     fontWeight: 900,
     zIndex: 1,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: theme.fontSizes.xs,
+      fontWeight: 300,
+    },
   },
 }));
 
 export function MealGenerator() {
+  //! below state just for testing purposes
+  const [showCarousel, setShowCarousel] = useState(false);
   const { classes } = useStyles();
   const {
     control,
@@ -42,53 +56,63 @@ export function MealGenerator() {
 
   const onSubmit: SubmitHandler<TMealGeneratorLandingSchema> = (data) => {
     console.log(data);
+    setShowCarousel(true);
     reset();
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Flex gap={50} justify="center" align="center" p={50}>
-        <Controller
-          name="meals"
-          control={control}
-          render={({ field }) => (
-            <Select
-              mt="md"
-              withinPortal
-              data={['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']}
-              placeholder="Pick one"
-              label="How many meals?"
-              classNames={classes}
-              size="lg"
-              // eslint-disable-next-line react/jsx-props-no-spreading
-              {...field}
-            />
-          )}
-        />
-        {errors.meals && <Text color="red">{`${errors.meals.message}`}</Text>}
-        <Controller
-          name="diet"
-          control={control}
-          render={({ field }) => (
-            <Select
-              mt="md"
-              withinPortal
-              data={['vegetarian', 'meat', 'vegan']}
-              placeholder="Pick one"
-              label="What's your diet?"
-              classNames={classes}
-              size="lg"
-              // eslint-disable-next-line react/jsx-props-no-spreading
-              {...field}
-            />
-          )}
-        />
-      </Flex>
-      <Flex justify="center" p={10} mb={10} pt={0}>
-        <Button color="green" size="xl" radius="xl" type="submit">
-          Generate
-        </Button>
-      </Flex>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Flex gap={50} justify="center" align="center" p={50}>
+          <Controller
+            name="meals"
+            control={control}
+            render={({ field }) => (
+              <Select
+                mt="md"
+                withinPortal
+                data={['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']}
+                placeholder="Select"
+                label="Meals"
+                classNames={classes}
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                {...field}
+              />
+            )}
+          />
+          {errors.meals && <Text color="red">{`${errors.meals.message}`}</Text>}
+          <Controller
+            name="diet"
+            control={control}
+            render={({ field }) => (
+              <Select
+                mt="md"
+                withinPortal
+                data={['vegetarian', 'meat', 'vegan']}
+                placeholder="Pick one"
+                label="Diet"
+                classNames={classes}
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                {...field}
+              />
+            )}
+          />
+        </Flex>
+        <Flex justify="center" p={10} mb={10} pt={0}>
+          <Button color="green" size="xl" radius="xl" type="submit">
+            Generate
+          </Button>
+        </Flex>
+      </form>
+      {showCarousel && (
+        <>
+          <MealsCarousel />
+          <Title align="center" pt={50} fw={900}>
+            Want to see more?
+          </Title>
+          <FeaturesOverview />
+        </>
+      )}
+    </>
   );
 }
