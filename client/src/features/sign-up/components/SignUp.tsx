fb3f83b531e042d0062e184e46aba8e1e6aaba82
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-09-01 11:05:01 +0300

    Add sign-up and login components, refactor routes

---
+++ client/src/features/sign-up/components/SignUp.tsx
@@ -0,0 +1,142 @@
+import { Controller, useForm, SubmitHandler } from 'react-hook-form';
+import { signUpSchema, TSignUpSchema } from '@shared/types';
+import { NavLink } from 'react-router-dom';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  TextInput,
+  PasswordInput,
+  Text,
+  Paper,
+  Group,
+  PaperProps,
+  Button,
+  Divider,
+  Checkbox,
+  Anchor,
+  Stack,
+  Box,
+  Title,
+  Container,
+} from '@mantine/core';
+import { GoogleButton, FacebookButton } from '@/components/Buttons';
+
+export function SignUp(props: PaperProps) {
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<TSignUpSchema>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<TSignUpSchema> = async (data) => {
+    console.log(data);
+
+    // just for mocking purposes, add server stuff later
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    reset();
+  };
+
+  return (
+    <Box
+      h="100vh"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingTop: '4rem',
+        gap: '2rem',
+      }}
+      component="main"
+    >
+      <Title>Create your mangify account</Title>
+      <Container size="xs" p="xl">
+        <Paper radius="md" p="xl" withBorder {...props}>
+          <Text size="xl" weight={500} align="center">
+            Sign in with
+          </Text>
+
+          <Group grow mb="md" mt="md">
+            <GoogleButton radius="xl">Google</GoogleButton>
+            <FacebookButton radius="xl">Facebook</FacebookButton>
+          </Group>
+
+          <Divider label="Or register with email" labelPosition="center" my="lg" />
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack>
+              <Controller
+                name="name"
+                control={control}
+                render={({ field }) => (
+                  <TextInput label="Name" placeholder="Your name" radius="md" {...field} />
+                )}
+              />
+              <Controller
+                name="email"
+                control={control}
+                render={({ field }) => (
+                  <TextInput
+                    label="Email"
+                    placeholder="example@google.com"
+                    radius="md"
+                    {...field}
+                  />
+                )}
+              />
+              {errors.email && <Text color="red" size="xs">{`${errors.email.message}`}</Text>}
+              <Controller
+                name="password"
+                control={control}
+                render={({ field }) => (
+                  <PasswordInput
+                    label="Password"
+                    placeholder="Your password"
+                    radius="md"
+                    {...field}
+                  />
+                )}
+              />
+              {errors.password && <Text color="red" size="xs">{`${errors.password.message}`}</Text>}
+              <Controller
+                name="confirmPassword"
+                control={control}
+                render={({ field }) => (
+                  <PasswordInput
+                    label="Confirm password"
+                    placeholder="Confirm your password"
+                    radius="md"
+                    {...field}
+                  />
+                )}
+              />
+              {errors.confirmPassword && (
+                <Text color="red" size="xs">{`${errors.confirmPassword.message}`}</Text>
+              )}
+
+              <Checkbox label="I accept the terms and conditions" />
+            </Stack>
+
+            <Group position="apart" mt="xl">
+              <Anchor component={NavLink} to="/login" color="dimmed" size="xs">
+                Already have an account? Login
+              </Anchor>
+              <Button type="submit" radius="xl" disabled={isSubmitting}>
+                Register
+              </Button>
+            </Group>
+          </form>
+        </Paper>
+      </Container>
+    </Box>
+  );
+}
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,11 +1,28 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Root } from './root';
+import { Landing } from './landing';
+import { SignUpRoute } from './sign-up';
+import { LoginRoute } from './login';
 
 export function AppRoutes() {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <Root />,
+      children: [
+        {
+          element: <Landing />,
+          index: true,
+        },
+        {
+          element: <LoginRoute />,
+          path: 'login',
+        },
+        {
+          element: <SignUpRoute />,
+          path: 'sign-up',
+        },
+      ],
     },
   ]);
 
