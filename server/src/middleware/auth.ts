9cc9f2631226c062a74a9d8c8ec2df367612d453
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-09-23 12:25:20 +0300

    Minor formatting changes in server middleware

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -4,15 +4,14 @@ export function isAuthenticated(req: Request, res: Response, next: NextFunction)
   const { user } = req.session;
   if (!user) {
     // below is to prevent a session being initialized if a request is unauthorized
-    // we do this because express-session is configured to run as a "global" middleware ATM
+    // we do this because express-session is configured to run as a "global" middleware for all endpoints ATM
     req.session.destroy(() => {});
-    return res.status(401).json({ message: 'Unauthorized' });
+    return res.status(401).json({ errors: 'Unauthorized' });
   }
 
-  //! do we even need the below? As we aren't refreshing the DB constantly
   if (user && user.disabled) {
     req.session.destroy(() => {});
-    return res.status(401).json({ message: 'User is disabled, please contact admin' });
+    return res.status(401).json({ errors: 'User is disabled, please contact admin' });
   }
   return next();
 }
@@ -24,5 +23,5 @@ export function isAdmin(req: Request, res: Response, next: NextFunction) {
     return next();
   }
   req.session.destroy(() => {});
-  return res.status(403).json({ message: 'Forbidden' });
+  return res.status(403).json({ errors: 'Forbidden' });
 }
