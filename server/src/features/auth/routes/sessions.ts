5de00eaad79f8e30993ef196f69d2f47ae63b3aa
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-09-24 23:20:59 +0300

    Add auth check and refresh token endpoints

--- server/src/features/auth/routes/sessions.ts
+++ server/src/features/auth/routes/sessions.ts
@@ -6,5 +6,7 @@ const sessionRouter = express.Router();
 
 sessionRouter.post('/login', sessionController.login as RequestHandler);
 sessionRouter.get('/logout', isAuthenticated, sessionController.logout);
+sessionRouter.get('/auth-check', sessionController.authCheck);
+sessionRouter.get('/refresh-session', sessionController.refreshSession);
 
 export { sessionRouter };
