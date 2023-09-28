3ee65bf5c9f5fa0d9f207653f7b093f2e70303bc
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-09-28 23:44:41 +0300

    Add all increment/decrement functionality to calendar slice

--- client/src/stores/slices/calendarSlice.ts
+++ client/src/stores/slices/calendarSlice.ts
@@ -7,7 +7,7 @@ import type { RootState } from '../store';
 import type { CalendarState } from '@/types';
 
 //* we store the dates as strings to avoid redux non-serializable error as Dates are inherently mutable
-//* and this goes against redux toolkit philosophy
+//* and this goes against redux philosophy
 
 const initialState: CalendarState = {
   day: true,
@@ -36,11 +36,38 @@ const calendarSlice = createSlice({
       const nextDay = currentDay.setDate(currentDay.getDate() + 1);
       state.dayRange = new Date(nextDay).toISOString();
     },
+    decrementDay: (state) => {
+      const currentDay = new Date(state.dayRange);
+      const prevDay = currentDay.setDate(currentDay.getDate() - 1);
+      state.dayRange = new Date(prevDay).toISOString();
+    },
+    incrementWeek: (state) => {
+      const currentWeekStart = new Date(state.weekRange[0]);
+      const nextWeek = currentWeekStart.setDate(currentWeekStart.getDate() + 7);
+      state.weekRange[0] = new Date(nextWeek).toISOString();
+      const endOfNextWeek = endOfWeek(nextWeek).toISOString();
+      state.weekRange[1] = endOfNextWeek;
+    },
+    decrementWeek: (state) => {
+      const currentWeekStart = new Date(state.weekRange[0]);
+      const prevWeek = currentWeekStart.setDate(currentWeekStart.getDate() - 7);
+      state.weekRange[0] = new Date(prevWeek).toISOString();
+      const endOfNextWeek = endOfWeek(prevWeek).toISOString();
+      state.weekRange[1] = endOfNextWeek;
+    },
   },
 });
 
-export const { setCalendar, setDayRange, setWeekStart, setWeekEnd, incrementDay } =
-  calendarSlice.actions;
+export const {
+  setCalendar,
+  setDayRange,
+  setWeekStart,
+  setWeekEnd,
+  incrementDay,
+  decrementDay,
+  incrementWeek,
+  decrementWeek,
+} = calendarSlice.actions;
 
 export const selectCalendar = (state: RootState) => state.calendar;
 
