405cc74f6aa48360640321b3ea7e67f1d92f9073
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-09-29 23:34:10 +0300

    Split dashboard sidebar into 2 comps to accommodate dropdown menu links

--- client/src/features/users/components/Sidebar.tsx
+++ client/src/features/users/components/Sidebar.tsx
@@ -5,73 +5,36 @@ import {
   IconSettings,
   IconChartPie2,
 } from '@tabler/icons-react';
-import { ThemeIcon, UnstyledButton, Group, Text } from '@mantine/core';
-import { NavLink } from 'react-router-dom';
-import { nanoid } from '@reduxjs/toolkit';
-
-interface MainLinkProps {
-  icon: React.ReactNode;
-  color: string;
-  label: string;
-  link: string;
-}
-
-function SideBarLinks({ icon, color, label, link }: MainLinkProps) {
-  return (
-    <UnstyledButton
-      sx={(theme) => ({
-        display: 'block',
-        width: '100%',
-        padding: theme.spacing.xs,
-        borderRadius: theme.radius.sm,
-        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
-
-        '&:hover': {
-          backgroundColor:
-            theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-        },
-      })}
-      component={NavLink}
-      to={link}
-    >
-      <Group>
-        <ThemeIcon color={color} variant="light">
-          {icon}
-        </ThemeIcon>
-
-        <Text size="sm">{label}</Text>
-      </Group>
-    </UnstyledButton>
-  );
-}
-
-const linkData = [
-  {
-    icon: <IconCalendar size="1rem" />,
-    color: 'teal',
-    label: 'Meal Planner',
-    link: '/',
-  },
-  { icon: <IconShoppingBag size="1rem" />, link: '', color: 'teal', label: 'Shopping List' },
-  { icon: <IconHeart size="1rem" />, link: '', color: 'teal', label: 'Favorites' },
-  {
-    icon: <IconChartPie2 size="1rem" />,
-    link: '',
-    color: 'teal',
-    label: 'Diet & Nutrition Preferences',
-  },
-  { icon: <IconSettings size="1rem" />, link: '/meal-settings', color: 'teal', label: 'Settings' },
-];
+import { SidebarSoloLink } from './SidebarSoloLinks';
 
 export function SideBar() {
-  const links = linkData.map((item) => (
-    <SideBarLinks
-      icon={item.icon}
-      link={item.link}
-      label={item.label}
-      color={item.color}
-      key={nanoid()}
-    />
-  ));
-  return <div>{links}</div>;
+  return (
+    <div>
+      <SidebarSoloLink
+        icon={<IconCalendar size="1rem" />}
+        color="teal"
+        label="Meal Planner"
+        link="/"
+      />
+      <SidebarSoloLink
+        icon={<IconShoppingBag size="1rem" />}
+        color="teal"
+        label="Shopping List"
+        link="/"
+      />
+      <SidebarSoloLink icon={<IconHeart size="1rem" />} color="teal" label="Favorites" link="/" />
+      <SidebarSoloLink
+        icon={<IconChartPie2 size="1rem" />}
+        color="teal"
+        label="Diet & Nutrition Preferences"
+        link="/nutrition-preferences"
+      />
+      <SidebarSoloLink
+        icon={<IconSettings size="1rem" />}
+        color="teal"
+        label="Settings"
+        link="/meal-settings"
+      />
+    </div>
+  );
 }
---
+++ client/src/features/users/components/SidebarSoloLinks.tsx
@@ -0,0 +1,38 @@
+import { ThemeIcon, UnstyledButton, Group, Text } from '@mantine/core';
+import { NavLink } from 'react-router-dom';
+
+type MainLinkProps = {
+  icon: React.ReactNode;
+  color: string;
+  label: string;
+  link: string;
+};
+
+export function SidebarSoloLink({ icon, color, label, link }: MainLinkProps) {
+  return (
+    <UnstyledButton
+      sx={(theme) => ({
+        display: 'block',
+        width: '100%',
+        padding: theme.spacing.xs,
+        borderRadius: theme.radius.sm,
+        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+
+        '&:hover': {
+          backgroundColor:
+            theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        },
+      })}
+      component={NavLink}
+      to={link}
+    >
+      <Group>
+        <ThemeIcon color={color} variant="light">
+          {icon}
+        </ThemeIcon>
+
+        <Text size="sm">{label}</Text>
+      </Group>
+    </UnstyledButton>
+  );
+}
