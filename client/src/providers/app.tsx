c6601eea057f4f02ccf588239061cc3a0a4b5e5a
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-10-15 00:50:20 +0300

    Add mantine notifications provider

--- client/src/providers/app.tsx
+++ client/src/providers/app.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useLocalStorage, useHotkeys } from '@mantine/hooks';
 import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
 import { Provider } from 'react-redux';
+import { Notifications } from '@mantine/notifications';
 import { store } from '@/stores';
 
 type AppProviderProps = {
@@ -25,6 +26,7 @@ export function AppProvider({ children }: AppProviderProps) {
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
       <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme }}>
         <Provider store={store}>{children}</Provider>
+        <Notifications />
       </MantineProvider>
     </ColorSchemeProvider>
   );
