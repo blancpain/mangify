ad98def096a36821824fc6456fcd81f88719c860
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-10-20 15:39:12 +0300

    refactor nutrition profile component

--- client/src/features/meal_planner/components/NutritionProfile.tsx
+++ client/src/features/meal_planner/components/NutritionProfile.tsx
@@ -1,8 +1,8 @@
-import { Box, Title } from '@mantine/core';
-import { Pie } from 'react-chartjs-2';
-import 'chart.js/auto'; // NOTE: needed when using chartjs w/ react
+import { Title } from '@mantine/core';
+import { nanoid } from '@reduxjs/toolkit';
 import { selectUser } from '@/stores';
 import { useAppSelector } from '@/hooks';
+import { PieChart } from '@/components';
 
 export function NutritionProfile() {
   const { profile } = useAppSelector(selectUser);
@@ -10,43 +10,14 @@ export function NutritionProfile() {
   return (
     <>
       {profile.calories && (
-        <>
-          <Title order={1} mb={20}>
-            Nutrition Profile
-          </Title>
-          <Title order={2} mb={20}>
-            Daily calories: {profile.calories}
-          </Title>
-          <Box h={350}>
-            <Pie
-              options={{
-                elements: {
-                  arc: {
-                    borderColor: '#E9ECEF',
-                  },
-                },
-                plugins: {
-                  legend: {
-                    labels: {
-                      boxHeight: 10,
-                      color: '#087F5B',
-                    },
-                  },
-                },
-              }}
-              data={{
-                labels: ['Protein', 'Carbs', 'Fats'],
-                datasets: [
-                  {
-                    data: [profile.protein, profile.carbs, profile.fats],
-                    backgroundColor: ['rgb(213, 45, 183)', 'rgb(96, 80, 220)', 'rgb(255, 107, 69)'],
-                    hoverOffset: 0,
-                  },
-                ],
-              }}
-            />
-          </Box>
-        </>
+        <PieChart
+          title="Your nutrition profile"
+          calories={profile.calories}
+          fats={profile.fats}
+          protein={profile.protein}
+          key={nanoid()}
+          carbs={profile.carbs}
+        />
       )}
       {!profile.calories && <Title>Please fill out your profile - click here...</Title>}
     </>
