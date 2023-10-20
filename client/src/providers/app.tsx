3456f4cfe46dae95108b6ffa0df09d7cb929bcea
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-10-20 15:37:52 +0300

    Add mantine breakpoints for better responsiveness

--- client/src/providers/app.tsx
+++ client/src/providers/app.tsx
@@ -24,7 +24,20 @@ export function AppProvider({ children }: AppProviderProps) {
 
   return (
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-      <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme }}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme,
+          breakpoints: {
+            xs: '30em',
+            sm: '48em',
+            md: '64em',
+            lg: '74em',
+            xl: '90em',
+          },
+        }}
+      >
         <Provider store={store}>{children}</Provider>
         <Notifications />
       </MantineProvider>
