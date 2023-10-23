2115df5040bb9aeaf2523a90990b2b0f133a8759
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-10-23 13:26:22 +0300

    Misc: minor fix auth sessions server routes

--- server/src/features/auth/routes/sessions.ts
+++ server/src/features/auth/routes/sessions.ts
@@ -6,7 +6,7 @@ const sessionRouter = express.Router();
 
 sessionRouter.post('/login', sessionController.login as RequestHandler);
 sessionRouter.get('/logout', isAuthenticated, sessionController.logout);
-sessionRouter.get('/auth-check', sessionController.authCheck);
-sessionRouter.get('/refresh-session', sessionController.refreshSession);
+sessionRouter.get('/auth-check', sessionController.authCheck as RequestHandler);
+sessionRouter.get('/refresh-session', sessionController.refreshSession as RequestHandler);
 
 export { sessionRouter };
