3232dc0a7b78c69b93400ad117edda0b65007814
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-11-06 11:23:32 +0200

    feat: add deployment script

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,20 @@ source /home/admin/.nvm/nvm.sh
 
 cd /home/admin/mangify
 
-cd frontend/
+cd client/
 npm install
 
-cd ../backend/
+cd ../server/
 npm install
 npm run build
 
 cd ..
 
-# remove old containers & images (volume should persist)
+# remove old containers & images (volumes should persist)
 docker compose down
 docker stop $(docker ps -aq)
 docker rm -f $(docker ps -aq)
-docker image prune -a -f
+docker volume rm $(docker volume ls -q)
+docker system prune -a
 
 docker compose up -d
