073b843e2de5ccc86a7e1b47a5ec47ce501a7e1f
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-11-06 17:16:10 +0200

    chore: add comment on cors in server/app

--- server/src/app.ts
+++ server/src/app.ts
@@ -47,7 +47,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173', // NOTE: can add more as needed
     credentials: true,
   }),
 );
