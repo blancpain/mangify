56c557d0c11620bd9a70841c7b033a26c108a632
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-11-16 09:39:14 +0200

    fix: add main course type to URL builder to ensure main courses returned

--- server/src/utils/url-builder.ts
+++ server/src/utils/url-builder.ts
@@ -14,14 +14,6 @@ export type TBuildURL = {
 export const buildURL = (args: TBuildURL): string => {
   // NOTE: using calory/macro targets does not produce very accurate results using the current API so we will let the user regenerate the meal(s) if they don't like them
   // we still show they targets and the meal data for informational purposes
-
-  // const caloriesTarget: number | null = args.calories
-  //   ? Number(args.calories / args.numberOfMeals)
-  //   : null;
-  // there aren't enough meals to also filter by macros at the moment
-  // const minCalories = caloriesTarget ? caloriesTarget - 300 : null;
-  // const maxCalories = caloriesTarget ? caloriesTarget + 100 : null;
-
   if (args.mealType === 'breakfast' || args.mealType === 'snack') {
     return `https://api.spoonacular.com/recipes/complexSearch?apiKey=${
       process.env.API_KEY
@@ -31,13 +23,12 @@ export const buildURL = (args: TBuildURL): string => {
       args.cuisine
     }&type=${args.mealType}`;
   }
-  // NOTE: we introduce minCalories for the main courses to make sure we are actually getting main courses and not small snacks/meals
-  // we also set maxCalories to ensure we aren't getting back meals that are overly calory dense
+  // NOTE: we add minCalories to ensure we actually get main courses
   return `https://api.spoonacular.com/recipes/complexSearch?apiKey=${
     process.env.API_KEY
   }&instructionsRequired=true&addRecipeInformation=true&addRecipeNutrition=true&fillIngredients=true&sort=random&number=${
     args.numberOfMeals
   }&diet=${args.diet?.toLowerCase()}&intolerances=${args.intolerances}&cuisine=${
     args.cuisine
-  }&minCalories=700&maxCalories=1100`;
+  }&type=${args.mealType}&minCalories=600`;
 };
